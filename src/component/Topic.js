5650fea936dbe8bc44a12a3739cfb56b0b90445d
taodtu <taodtu@gmail.com>
2019-06-16 13:36:35 +0100

    implement topic component

--- src/component/Topic.js
+++ src/component/Topic.js
@@ -2,12 +2,33 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 
-const GET_TOPICS = `
+const GET_TOPICS = gql`
 {
   topics{
     slug
     description
     article_count
     comment_count
+    articles {
+     article_id
+    }
   }
 }`;
+const Topic = ({ onTopicSelected }) => (
+ <Query query={GET_TOPICS} >
+  {({ loading, error, data }) => {
+   if (loading) return "Loading...";
+   if (error) return `Error! ${error.message}`;
+
+   return (
+    <select name="topic" onChange={onTopicSelected}>
+     {data.topics.map(topic => (
+      <option key={topic.slug} value={topic.slug}>
+       {topic.slug}
+      </option>
+     ))}
+    </select>
+   );
+  }}
+ </Query>);
+export default Topic
