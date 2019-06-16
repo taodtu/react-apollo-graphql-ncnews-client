b9be8a765730a3d6c42b3613672b3b43663f5061
taodtu <taodtu@gmail.com>
2019-06-16 17:16:11 +0100

    implement the topic and topicitem component

--- src/component/Topic.js
+++ src/component/Topic.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import TopicItem from './TopicItem';
 
 const GET_TOPICS = gql`
 {
@@ -9,26 +10,49 @@ const GET_TOPICS = gql`
     description
     article_count
     comment_count
-    articles {
+    articles{
      article_id
-    }
+     title
+     body
+     votes
+     topic
+     author
+     created_at
+    user {
+      username,
+      name}
+  comment_count}
   }
 }`;
-const Topic = ({ onTopicSelected }) => (
- <Query query={GET_TOPICS} >
-  {({ loading, error, data }) => {
-   if (loading) return "Loading...";
-   if (error) return `Error! ${error.message}`;
+class Topic extends Component {
+ state = { slugSelected: null }
+ onTopicSelected = ({ target }) => {
+  this.setState(() => ({ slugSelected: target.value }));
+ };
 
-   return (
-    <select name="topic" onChange={onTopicSelected}>
-     {data.topics.map(topic => (
-      <option key={topic.slug} value={topic.slug}>
-       {topic.slug}
-      </option>
-     ))}
-    </select>
-   );
-  }}
- </Query>);
+ render() {
+  return (<Query query={GET_TOPICS} >
+   {({ loading, error, data }) => {
+    if (loading) return "Loading...";
+    if (error) return `Error! ${error.message}`;
+    const { topics } = data;
+    return (
+     <div>
+      <h3>Topics</h3>
+      <select name="topic" onChange={this.onTopicSelected}>
+       <option value=''>Please select a topic</option>
+       {topics.map(topic => (
+        <option key={topic.slug} value={topic.slug}>
+         {topic.slug}
+        </option>
+       ))}
+      </select>
+      <TopicItem topic={topics.filter(topic => topic.slug === this.state.slugSelected)[0]} />
+     </div>
+    );
+   }}
+  </Query>
+  )
+ }
+};
 export default Topic
