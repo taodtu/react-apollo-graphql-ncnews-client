8e9c579e45b7ca5416775e927bfad1aa755f2475
taodtu <taodtu@gmail.com>
2019-06-16 10:29:01 +0100

    install individual package instead of apollo-boost

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const httpLink = new HttpLink({
+ uri: "https://nc-news-graphql-server.herokuapp.com/graphql"
+})
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
