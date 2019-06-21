15360e5fffec391404e0594af2621474aeb915ef
taodtu <taodtu@gmail.com>
2019-06-21 15:12:36 +0100

    fix

--- src/component/ArticleItem.js
+++ src/component/ArticleItem.js
@@ -12,40 +12,40 @@ mutation UpdateArticle ($article_id: ID! $inc_votes: Int! ) {
  }
 }`
 const updateVotes = (client, { data: { updateArticle: { article_id } } }) => {
- const article = client.readFragment({
-  id: `Article:${article_id}`,
-  fragment: ARTICLE_FRAGMENT
- })
- console.log(client)
- // const votes = article.votes + 1;
- // client.writeQuery({
- //  query: GET_TOPICS,
- //  data: {
- //   ...article,
- //   votes
- //  }
- // })
+  const article = client.readFragment({
+    id: `Article:${article_id}`,
+    fragment: ARTICLE_FRAGMENT
+  })
+
+  // const votes = article.votes + 1;
+  // client.writeQuery({
+  //  query: GET_TOPICS,
+  //  data: {
+  //   ...article,
+  //   votes
+  //  }
+  // })
 }
 const ArticleItem = ({ article_id, author, votes, created_at, comment_count }) => {
- return (
-  <div className="topic">
-   <span style={{ width: '10%' }}>{article_id} </span>
-   <span style={{ width: '20%' }}>{author} </span>
-   <div style={{ width: '30%' }}>
-    <Mutation
-     mutation={UPDATE_VOTES}
-     variables={{ "article_id": article_id, "inc_votes": 1 }}
-     // refetchQueries={[{ query: GET_TOPICS }]}
-     update={updateVotes}
-    >
-     {(updateArticle, { data, loading, error }) => (
-      <div> {data ? data.updateArticle.votes : votes}<button onClick={updateArticle} >
-       +</button> </div>)}
-    </Mutation>
-   </div>
-   <span style={{ width: '20%' }}>{created_at} </span>
-   <span style={{ width: '20%' }}>{comment_count} </span>
-  </div>)
+  return (
+    <div className="topic">
+      <span style={{ width: '10%' }}>{article_id} </span>
+      <span style={{ width: '20%' }}>{author} </span>
+      <div style={{ width: '30%' }}>
+        <Mutation
+          mutation={UPDATE_VOTES}
+          variables={{ "article_id": article_id, "inc_votes": 1 }}
+          // refetchQueries={[{ query: GET_TOPICS }]}
+          update={updateVotes}
+        >
+          {(updateArticle, { data, loading, error }) => (
+            <div> {data ? data.updateArticle.votes : votes}<button onClick={updateArticle} >
+              +</button> </div>)}
+        </Mutation>
+      </div>
+      <span style={{ width: '20%' }}>{created_at} </span>
+      <span style={{ width: '20%' }}>{comment_count} </span>
+    </div>)
 
 }
 export default ArticleItem;
