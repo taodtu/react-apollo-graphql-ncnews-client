b3efe9042cbf30c9ef52a238536ab1966ae43d40
taodtu <taodtu@gmail.com>
2019-06-22 13:49:08 +0100

    add query

--- src/component/ArticlePage.js
+++ src/component/ArticlePage.js
@@ -1,4 +1,19 @@
 import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const GET_ARTICLE = gql`
+query($id: ID!)
+  {getArticle(article_id:$id ) {
+    article_id
+    title,
+    body,
+    author,
+    topic,
+    votes,
+    created_at,
+    comment_count,
+}}`;
 
 class ArticlePage extends Component {
  render() {
