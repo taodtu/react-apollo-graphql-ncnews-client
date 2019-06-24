f8fb8efab8b7f9ae3a063d4554b9e2b66aaff60b
taodtu <taodtu@gmail.com>
2019-06-24 16:42:23 +0100

    implement user select instead of input box

--- src/component/comment/AddComment.js
+++ src/component/comment/AddComment.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { ADD_COMMENT } from '../../constant/Mutation';
 import { Mutation } from 'react-apollo';
 import { GET_ARTICLE } from '../../constant/Query';
-import { Button, TextField } from '@material-ui/core';
+import { Button, TextField, Select, MenuItem } from '@material-ui/core';
 import Style from './AddComment.module.css';
 
 const INITIAL_STATE = {
- author: '',
+ author: 'jessjelly',
  body: ''
 }
 
@@ -15,12 +15,20 @@ class AddComment extends Component {
   ...INITIAL_STATE
  };
  onChange = ({ target }) => {
-  const { name, value } = target;
+  const { value } = target;
   this.setState({
    ...this.state,
-   [name]: value
+   body: value
   })
  }
+ handleChange = ({ target }) => {
+  const { value } = target;
+  this.setState({
+   ...this.state,
+   author: value
+  })
+ }
+
  handleSubmit = (e, createComment) => {
   const { author, body } = this.state;
   const { id } = this.props;
@@ -42,7 +50,7 @@ class AddComment extends Component {
      const { getArticle } = cache.readQuery({ query: GET_ARTICLE, variables: { id } });
      const { comments, comment_count } = getArticle;
      const newCount = comment_count + 1;
-     const newComments = [...comments, createComment]
+     const newComments = [createComment, ...comments]
      cache.writeQuery({
       query: GET_ARTICLE,
       variables: { id },
@@ -59,10 +67,18 @@ class AddComment extends Component {
        <form onSubmit={(e) => this.handleSubmit(e, createComment)} >
         <div className={Style.form}>
          <div className={Style.username}>
-          <TextField name='author' label='username' value={author} onChange={this.onChange} margin='none' />
+          <Select value={author} autoWidth={true} onChange={this.handleChange}
+          >
+           <MenuItem value="jessjelly">jessjelly</MenuItem>
+           <MenuItem value="tickle122">tickle122</MenuItem>
+           <MenuItem value="grumpy19">grumpy19</MenuItem>
+           <MenuItem value="happyamy2016">happyamy2016</MenuItem>
+           <MenuItem value="cooljmessy">cooljmessy</MenuItem>
+           <MenuItem value="weegembump">weegembump</MenuItem>
+          </Select>
          </div>
          <div className={Style.body}>
-          <TextField name='body' label='comment' value={body} onChange={this.onChange} margin='none' fullWidth />
+          <TextField label='comment' value={body} onChange={this.onChange} margin='none' fullWidth />
          </div>
          <div className={Style.submit}>
           <Button type="submit" variant="contained" size="medium" color="primary" disabled={!(author && body)}> Submit </Button>
