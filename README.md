409670e55dea3540567554c829f084f17767fc67
taodtu <taodtu@gmail.com>
2019-06-29 12:10:34 +0100

    Implement React Hooks on Topic/User Item

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ Northcoders News is a social news aggregation, web content rating, and discussio
 
 - All graphQL mutations are achieved using **`Apollo Cache`**, rather than refetching from the server after mutation.
 
+- With implementation of **`React Hooks`**, all components are writen as **`functional stateless`**.
+
 - **`Optimistic response`** is implemented on article vote button.
 
 - **`Prefetching data`** is applied on Topic/User list item. So the articles data is fetched and stored in cache when mouse is hovering on it, and show immediately when user click on it. This feature is not working on Mobile device while no onMouseOver is enabled.
--- src/component/topic/TopicItem.js
+++ src/component/topic/TopicItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Style from './TopicItem.module.css';
 import { Query } from 'react-apollo';
 import { GET_ARTICLES_TOPIC } from '../../constant/Query'
@@ -6,92 +6,85 @@ import ArticleItem from '../article/ArticleItem';
 import { Button } from '@material-ui/core';
 import Loader from 'react-loader-spinner';
 
-class TopicItem extends Component {
-  state = {
-    clicked: false,
+const TopicItem = ({ topic, client }) => {
+  const [clicked, setClick] = useState(false)
+  const handleClick = (clicked) => {
+    setClick(!clicked)
   }
-  handleClick = (clicked) => {
-    this.setState({
-      clicked: !clicked,
-    })
-  }
-  render() {
-    const { topic } = this.props;
-    const { slug } = topic;
-    const { clicked } = this.state;
-    return (
-      <div>
-        <div className={Style.topic}
-          onClick={() => this.handleClick(clicked)}
-          onMouseOver={() =>
-            this.props.client.query({
-              query: GET_ARTICLES_TOPIC,
-              variables: { slug, offset: 0, limit: 3 }
-            })
-          }
-        >
-          <strong className={Style.item} style={{ "textAlign": "center", "marginTop": "0.8em" }}>Slug:  {slug} </strong>
-          <p className={Style.item}>Description:  {topic.description} </p>
-          <p className={Style.item} >Article_count: {topic.article_count} Comment_count: {topic.comment_count}</p>
-        </div>
-        {clicked && <Query query={GET_ARTICLES_TOPIC}
-          variables={{ slug, offset: 0, limit: 3 }}
-          notifyOnNetworkStatusChange={true} >
-          {({ error, loading, data, fetchMore }) => {
-            if (loading) return <Loader
-              type="Puff"
-              color="#00BFFF"
-              height="100"
-              width="100"
-            />;
-            if (error) return `Error! ${error.message}`;
-            const { articlesByTopic } = data;
-            return (
-              <div>
-                <h4>Articles belong to {slug}, click title to see more details</h4>
-                {articlesByTopic && <div className={Style.articles} >
-                  {articlesByTopic.map(article => <ArticleItem article={article} key={article.article_id} />)}
-                </div>}
-
-                <div className={Style.button} >
-                  <div className={Style.buttonitem}>
-                    <Button variant="outlined" size="medium" color="primary"
-                      onClick={() =>
-                        fetchMore({
-                          variables: {
-                            offset: articlesByTopic.length
-                          },
-                          updateQuery: (prev, { fetchMoreResult }) => {
-                            if (!fetchMoreResult) return prev;
-                            return { ...prev, articlesByTopic: [...prev.articlesByTopic, ...fetchMoreResult.articlesByTopic] }
-                          }
-                        })}
-                    > More </Button>
-                  </div>
-                  <div className={Style.buttonitem}>
-                    <Button variant="outlined" size="medium" color="primary"
-                      onClick={() =>
-                        fetchMore({
-                          variables: {
-                            offset: articlesByTopic.length,
-                            limit: null
-                          },
-                          updateQuery: (prev, { fetchMoreResult }) => {
-                            if (!fetchMoreResult) return prev;
-                            return { ...prev, articlesByTopic: [...prev.articlesByTopic, ...fetchMoreResult.articlesByTopic] }
-                          }
-                        })}
-                    > Show all </Button></div>
-                  <Button variant="outlined" size="medium" color="secondary"
-                    onClick={() => this.handleClick(clicked)}
-                  > Close all </Button>
-                </div>
-              </div>
-            );
-          }}
-        </Query>}
+  const { slug } = topic;
+  return (
+    <div>
+      <div className={Style.topic}
+        onClick={() => handleClick(clicked)}
+        onMouseOver={() =>
+          client.query({
+            query: GET_ARTICLES_TOPIC,
+            variables: { slug, offset: 0, limit: 3 }
+          })
+        }
+      >
+        <strong className={Style.item} style={{ "textAlign": "center", "marginTop": "0.8em" }}>Slug:  {slug} </strong>
+        <p className={Style.item}>Description:  {topic.description} </p>
+        <p className={Style.item} >Article_count: {topic.article_count} Comment_count: {topic.comment_count}</p>
       </div>
-    )
-  }
+      {clicked && <Query query={GET_ARTICLES_TOPIC}
+        variables={{ slug, offset: 0, limit: 3 }}
+        notifyOnNetworkStatusChange={true} >
+        {({ error, loading, data, fetchMore }) => {
+          if (loading) return <Loader
+            type="Puff"
+            color="#00BFFF"
+            height="100"
+            width="100"
+          />;
+          if (error) return `Error! ${error.message}`;
+          const { articlesByTopic } = data;
+          return (
+            <div>
+              <h4>Articles belong to {slug}, click title to see more details</h4>
+              {articlesByTopic && <div className={Style.articles} >
+                {articlesByTopic.map(article => <ArticleItem article={article} key={article.article_id} />)}
+              </div>}
+
+              <div className={Style.button} >
+                <div className={Style.buttonitem}>
+                  <Button variant="outlined" size="medium" color="primary"
+                    onClick={() =>
+                      fetchMore({
+                        variables: {
+                          offset: articlesByTopic.length
+                        },
+                        updateQuery: (prev, { fetchMoreResult }) => {
+                          if (!fetchMoreResult) return prev;
+                          return { ...prev, articlesByTopic: [...prev.articlesByTopic, ...fetchMoreResult.articlesByTopic] }
+                        }
+                      })}
+                  > More </Button>
+                </div>
+                <div className={Style.buttonitem}>
+                  <Button variant="outlined" size="medium" color="primary"
+                    onClick={() =>
+                      fetchMore({
+                        variables: {
+                          offset: articlesByTopic.length,
+                          limit: null
+                        },
+                        updateQuery: (prev, { fetchMoreResult }) => {
+                          if (!fetchMoreResult) return prev;
+                          return { ...prev, articlesByTopic: [...prev.articlesByTopic, ...fetchMoreResult.articlesByTopic] }
+                        }
+                      })}
+                  > Show all </Button></div>
+                <Button variant="outlined" size="medium" color="secondary"
+                  onClick={() => handleClick(clicked)}
+                > Close all </Button>
+              </div>
+            </div>
+          );
+        }}
+      </Query>}
+    </div>
+  )
 }
+
 export default TopicItem;
